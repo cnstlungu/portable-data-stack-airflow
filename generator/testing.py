504a38fdb623d9f8a69628b4063f8721757baeb4
Constantin Lungu <lungupconstantin@gmail.com>
2021-05-12 01:03:28 +0300

    remove extra file

--- generator/testing.py
+++
@@ -1,8 +0,0 @@
-CHANNEL_DISTRIBUTION = {'direct':(4,2,4), 'reseller': (1,4,5)}
-
-def get_channel_distribution(seller):
-    if seller in CHANNEL_DISTRIBUTION.keys():
-        distr = CHANNEL_DISTRIBUTION[seller]
-        return [*distr[0]*('in-store',), *distr[1]*('web',), *distr[2]*('mobile app',)]
-
-print(get_channel_distribution('direct'))
