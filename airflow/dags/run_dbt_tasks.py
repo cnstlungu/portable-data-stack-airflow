c07b332ee8098ac110159fb59dc6c47d1ea14f4c
Constantin Lungu <lungupconstantin@gmail.com>
2022-08-31 23:09:53 +0300

    Enable dbt DAG

--- airflow/dags/run_dbt_tasks.py
+++ airflow/dags/run_dbt_tasks.py
@@ -1,22 +1,39 @@
+from datetime import timedelta, datetime
 from airflow import DAG
+import pytz
 
-from datetime import datetime
-from airflow.utils import timezone
+from airflow.utils.dates import days_ago
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
 from airflow_dbt.operators.dbt_operator import (
     DbtSeedOperator,
     DbtRunOperator,
     DbtTestOperator
 )
 
-now = timezone.utcnow()
-
 default_args = {
   'dir': '/usr/local/airflow/dbt',
-  'start_date': timezone.datetime(2021,1,1),
+  'start_date': days_ago(1),
   'dbt_bin': '/usr/local/airflow/.local/bin/dbt'
 }
 
 with DAG(dag_id='run_dbt_tasks', default_args=default_args, schedule_interval='@daily', ) as dag:
+
+  wait_for_main = ExternalTaskSensor(
+    task_id='wait_for_main',
+    external_dag_id='import_main_data',
+    execution_date_fn = lambda x: days_ago(1),
+    timeout=1,
+    dag=dag
+)
+
+  wait_for_resellers = ExternalTaskSensor(
+    task_id='wait_for_resellers',
+    external_dag_id='import_reseller_data',
+    execution_date_fn = lambda x: days_ago(1),
+    timeout=1,
+    dag=dag
+)
+
 
   dbt_seed = DbtSeedOperator(
     task_id='dbt_seed',
@@ -32,4 +49,4 @@ with DAG(dag_id='run_dbt_tasks', default_args=default_args, schedule_interval='@
 
 
 
-  dbt_seed >> dbt_run >> dbt_test
+  wait_for_main >> wait_for_resellers >> dbt_seed >> dbt_run >> dbt_test
